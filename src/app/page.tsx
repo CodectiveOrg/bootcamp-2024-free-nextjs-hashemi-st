b5e608bb72cb3310914040747e0d4e44f3335d4a
hashemi <hashemi.st@gmail.com>
2024-12-21 00:58:58 +0330

    [ADD]footer [ADD]searchbox [ADD]logo&&icones [UPDATE]page.tsx [UPDATE]favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,23 @@
-export default async function Home() {
+import GlobalSearchBoxComponent from "@/components/global-search-box/global-search-box.component";
+import Logo from "@/logo/Logo";
 
+import styles from "./page.module.css";
+
+export default async function Home() {
   return (
-    <main>
-      <h1>سلام دنیا!</h1>
-    </main>
-  );
+    <div className={styles.home}>
+      <h1>
+        <Logo />
+         چکاپ &nbsp;فایند 
+      </h1>
+      <GlobalSearchBoxComponent />
+      <div className={styles.history}>
+        <div className={styles.title}>آخرین جستجوهای شما</div>
+        <ul>
+          <li>ارتوپد</li>
+          <li>قلب و عروق</li>
+        </ul>
+      </div>
+    </div>
+  )
 }
