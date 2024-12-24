8834b8e10b99ecb8a3fa5d54d958e510c6740044
hashemi-st <43073702+hashemi-st@users.noreply.github.com>
2024-12-24 17:32:39 +0330

    Lesson 2 (#3)
    * [ADD] prettier
    
    * [ADD] lint.yml File
    
    * [ADD] postcss.config.json File
    
    * [ADD] Reset css codes & variables & dark mode in global.css File
    
    * [ADD] header
    
    * [ADD]footer [ADD]searchbox [ADD]logo&&icones [UPDATE]page.tsx [UPDATE]favicon
    
    * remove needless files
    
    * [fixed] errors of lint
    
    * [INSTALL] prettier
    
    * [prettier fix]

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,19 @@
 import type { Metadata } from "next";
-import { Vazirmatn } from 'next/font/google';
+import { Vazirmatn } from "next/font/google";
+
+import HeaderComponent from "@/components/header/header.component";
+import FooterComponent from "@/components/footer/footer.component";
+
 import "./globals.css";
 
 const vazirmatn = Vazirmatn({
-  subsets: ['latin','arabic'],
-  display: 'swap',
-})
+  subsets: ["latin", "arabic"],
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: "چکاپ فایند",
-  description:"پلتفرم جامع جستجوی دکتر و رزرو نوبت آنلاین",
+  description: "پلتفرم جامع جستجوی دکتر و رزرو نوبت آنلاین",
 };
 
 export default function RootLayout({
@@ -20,7 +24,12 @@ export default function RootLayout({
   return (
     <html lang="fa" dir="rtl" className={vazirmatn.className}>
       <body>
-        {children}
+        <HeaderComponent />
+        <main>{children}</main>
+        <p className="tagline">
+          نوبت دهی پزشکی، سامانه نوبت دهی اینترنتی بیمارستان و پزشکان
+        </p>
+        <FooterComponent />
       </body>
     </html>
   );
---
+++ src/components/footer/footer.component.tsx
@@ -0,0 +1,76 @@
+import { ReactElement } from "react";
+
+import Link from "next/link";
+import Image from "next/image";
+
+import certificateLogo from "@/assets/logo/certificate.svg";
+import enamadLogo from "@/assets/logo/enamad.svg";
+import idkLogo from "@/assets/logo/idk.svg";
+import MingcuteTelegramFill from "@/icones/MingcuteTelegramFill";
+import MingcuteLinkedinFill from "@/icones/MingcuteLinkedinFill";
+import MingcuteYoutubeFill from "@/icones/MingcuteYoutubeFill";
+
+import styles from "./footer.module.css";
+
+export default function FooterComponent(): ReactElement {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.writings}>
+        <div className={styles.logo}>چکاپ فایند</div>
+        <p className={styles.description}>
+          بهترین مرجع جستجوی پزشکان سراسر کشور
+        </p>
+      </div>
+      <div className={styles.visuals}>
+        <ul className={styles.certificates}>
+          <li>
+            <Link href="#">
+              <Image src={idkLogo} alt="idk logo" />
+            </Link>
+          </li>
+          <li>
+            <Link href="#">
+              <Image src={certificateLogo} alt="certificate logo" />
+            </Link>
+          </li>
+          <li>
+            <Link href="#">
+              <Image src={enamadLogo} alt="enamad logo" />
+            </Link>
+          </li>
+        </ul>
+        <ul className={styles.socials}>
+          <li>
+            <Link href="https://t.me/Codective" target="_blank">
+              <MingcuteTelegramFill />
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="https://www.linkedin.com/in/bijanprogrammer/"
+              target="_blank"
+            >
+              <MingcuteLinkedinFill />
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="https://www.youtube.com/@BijanProgrammer"
+              target="_blank"
+            >
+              <MingcuteYoutubeFill />
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <p className={styles.copy}>
+        تمامی حقوق مادی و معنوی این وبسایت، خدمات و محتوای مربوط به آن متعلق به
+        سایت{" "}
+        <Link style={{ color: "var(--color-primary)" }} href="/">
+          چکاپ فایند
+        </Link>{" "}
+        می باشد.
+      </p>
+    </footer>
+  );
+}
