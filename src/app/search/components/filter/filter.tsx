570c4680fa77c51566e50794c9c4fe279e779e19
hashemi <hashemi.st@gmail.com>
2025-01-06 00:05:16 +0330

    [ADD] sidebar filter into search page

---
+++ src/app/search/components/filter/filter.tsx
@@ -0,0 +1,127 @@
+
+import styles from "./filter.module.css";
+
+
+export default function Filter() {
+
+    return (
+        <div className={styles.filterWrapper}>
+            <div className={styles.selectfilterWrapper}>
+                <div className={styles.wrapper}>
+                    <span>فیلترها</span>
+                    <span className={styles.delete}>حذف همه</span>
+                </div>
+                <div className={styles.labels}>
+                    <div className={styles.label}>
+                        <div>قلب</div><span>X</span>
+                    </div>
+                    <div className={styles.label}>
+                        <div>تهران</div><span>X</span>
+                    </div>
+                    <div className={styles.label}>
+                        <div>مرد</div><span>X</span>
+                    </div>
+
+                </div>
+            </div>
+
+            <div className={styles.wrapperExpert}>
+                <div className={styles.titleExpert}>تخصص :</div>
+                <div className={styles.experts}>
+                    <label>
+                        <input type="checkbox" value="heart" />
+                        قلب
+                    </label>
+                    <label>
+                        <input type="checkbox" value="brain" />
+                        مغز
+                    </label>
+                    <label>
+                        <input type="checkbox" value="ear" />
+                        گوش
+                    </label>
+                    <label>
+                        <input type="checkbox" value="eye" />
+                        چشم
+                    </label>
+                    <label>
+                        <input type="checkbox" value="kedney" />
+                        کلیه
+                    </label>
+                    <label>
+                        <input type="checkbox" value="internal" />
+                        داخلی
+                    </label>
+                    <label>
+                        <input type="checkbox" value="skin" />
+                        پوست
+                    </label>
+                    <label>
+                        <input type="checkbox" value="spychology" />
+                        روانپزشکی
+                    </label>
+                    <label>
+                        <input type="checkbox" value="ortopedy" />
+                        ارتوپدی
+                    </label>
+                </div>
+            </div>
+
+            <div className={styles.wrapperCity}>
+                <div>انتخاب شهر </div>
+                <select className={styles.city}>
+                    <option></option>
+                    <option>البرز</option>
+                    <option>اراک</option>
+                    <option>تهران</option>
+                    <option>تبریز</option>
+                </select>
+            </div>
+
+            <div className={styles.wrapperGender}>
+                <div>جنسیت :</div>
+                <div className={styles.radio}>
+                    <label>
+                        <input type="radio" id="man" value="man" />
+                        مرد
+                    </label>
+                    <label>
+                        <input type="radio" id="woman" value="woman" />
+                        زن
+                    </label>
+                </div>
+            </div>
+
+            <div className={styles.wrapperPoints}>
+                <div>امتیاز</div>
+                <div className={styles.radio}>
+                    <label>
+                        <input type="radio" value="man" />
+                        بالای 4
+                    </label>
+                    <label>
+                        <input type="radio" value="woman" />
+                        3 تا 4
+                    </label>
+                    <label>
+                        <input type="radio" value="woman" />
+                        زیر 3
+                    </label>
+                </div>
+            </div>
+            <div className={styles.wrapperOther}>
+                <div>سایر</div>
+                <div className={styles.radio}>
+                    <label>
+                        <input type="radio" value="man" />
+                        خوش برخورد
+                    </label>
+                    <label>
+                        <input type="radio" value="woman" />
+                        بدون معطلی
+                    </label>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,11 +1,11 @@
 import styles from './page.module.css'
 import DoctorList from './components/doctor-list/doctorList'
-
+import Filter from './components/filter/filter'
 export default function search() {
     return (
         <div className={styles.page}>
             <div className={styles.filters}>
-                SideBar
+                <Filter />
             </div>
             <DoctorList />
         </div>
