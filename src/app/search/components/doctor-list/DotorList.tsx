cd9f6d4b6027bed81858542da618d61ba7b3e55d
hashemi <hashemi.st@gmail.com>
2025-01-25 20:30:17 +0330

    Resolving PR comments

--- src/app/search/components/doctor-list/DotorList.tsx
+++ src/app/search/components/doctor-list/DotorList.tsx
@@ -6,19 +6,21 @@ import styles from "./DoctorList.module.css";
 export default function DoctorList() {
   const { filteredDoctors } = useContext(DoctorsContext);
 
+  if (filteredDoctors.length === 0) {
+    return (
+      <div className={styles.emptyList}>
+        <h3>متاسفانه هیچ دکتری با تخصص موردنظر یافت نشد.</h3>
+      </div>
+    );
+  }
+
   return (
     <div className={styles.list}>
-      {filteredDoctors.length ? (
-        filteredDoctors.map((doctor) => (
-          <div key={doctor.id}>
-            <Doctor doctor={doctor} />
-          </div>
-        ))
-      ) : (
-        <div className={styles.emptyList}>
-          <h3>متاسفانه هیچ دکتری با تخصص موردنظر یافت نشد.</h3>
+      {filteredDoctors.map((doctor) => (
+        <div key={doctor.id}>
+          <Doctor doctor={doctor} />
         </div>
-      )}
+      ))}
     </div>
   );
 }
