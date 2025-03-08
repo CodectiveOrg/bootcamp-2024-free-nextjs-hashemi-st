8bcca8888416db51d3f48b8d1c757c62febcb37e
hashemi <hashemi.st@gmail.com>
2025-03-09 02:46:58 +0330

    [ADD] error handling in sign-up route

--- src/app/api/auth/sign-up/route.ts
+++ src/app/api/auth/sign-up/route.ts
@@ -1,13 +1,17 @@
 import prisma from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
-import { tryCatch } from "@/utils/api.utils";
+import { tryCatch, parseBody } from "@/utils/api.utils";
 import { ApiResponseType } from "@/types/api-response.type";
 
-export async function POST(request: NextRequest): Promise<ApiResponseType<null>> {
- return tryCatch(async () => {
-    const body = await request.json();
-
-    await prisma.user.create({ data: body });
-    return NextResponse.json({ data: null , status: 201 });
+export async function POST(
+  request: NextRequest
+): Promise<ApiResponseType<null>> {
+  return tryCatch(async () => {
+    const [ParseError, body] = await parseBody<any>(request);
+    if (ParseError) {
+      return NextResponse.json({ error:ParseError}, {status: 400 });
+    }
+    await prisma.user.create({data:body});
+    return NextResponse.json({ data: null, status: 201 });
   });
 }
--- src/utils/api.utils.ts
+++ src/utils/api.utils.ts
@@ -1,15 +1,53 @@
 import { ApiResponseType } from "@/types/api-response.type";
 import { NextResponse } from "next/server";
 
+type ParseBodyResult<T> = [error: null, data: T] | [error: string, data: null];
+
+export async function parseBody<T>(
+  request: Request
+): Promise<ParseBodyResult<T>> {
+  try {
+    const body = await request.json();
+    return [null, body];
+  } catch (error) {
+    if (error instanceof Error) {
+      if (error.name === "SyntaxError") {
+        return ["فرمت اشتباه است", null];
+      }
+      return [error.message, null];
+    }
+    if (typeof error === "string") {
+      return [error, null];
+    }
+    return ["خطای غیر منتظره ", null];
+  }
+}
+
 export async function tryCatch<T>(
   callback: () => Promise<ApiResponseType<T>>
 ): Promise<ApiResponseType<T>> {
   try {
     return await callback();
-  } catch (error) {
+  } catch (error) { 
+   
     if (error instanceof Error) {
+      console.log('name',error.name)
+      if (error.name === "PrismaClientValidationError") {
+        return NextResponse.json(
+          { error: "همه پارامترهای مورد نیاز پاس داده نشده است." },
+          { status: 400 }
+        );
+      }
+      if (error.name === "PrismaClientInitializationError") {
+        return NextResponse.json(
+          { error: "متاسفانه ارتباط با دیتابیس برقرار نشد." },
+          { status: 400 }
+        );
+      }
+      
       return NextResponse.json({ error: error.message }, { status: 400 });
     }
+
     return NextResponse.json({ error: "خطای غیر منتظره" }, { status: 500 });
   }
 }
